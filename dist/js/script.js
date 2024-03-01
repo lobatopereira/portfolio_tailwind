de476efa74319088cb5d930110f021dee272f923
Lourenco Pereira <53714683+lobatopereira@users.noreply.github.com>
2024-03-01 09:53:21 +0900

    aumenta dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,17 @@
 window.onscroll = function () {
    const header = document.querySelector('header');
    const fixedNav = header.offsetTop;
+   const toTop = document.querySelector('#to-top');
 
    if (window.pageYOffset > fixedNav) {
       header.classList.add('navbar-fixed');
+      toTop.classList.remove('hidden');
+      toTop.classList.add('flex');
+
    }else{
       header.classList.remove('navbar-fixed');
+      toTop.classList.add('hidden');
+      toTop.classList.remove('flex');
    }
 }
 
@@ -17,3 +23,33 @@ hamburger.addEventListener('click', function () {
    hamburger.classList.toggle('hamburger-active');
    navmenu.classList.toggle('hidden');
 });
+
+// klik diluar hamburger
+window.addEventListener('click',function (e) {
+   if (e.target!= hamburger && e.target != navmenu) { 
+      hamburger.classList.remove('hamburger-active');
+      navmenu.classList.add('hidden');
+   }
+})
+
+
+// dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+darkToggle.addEventListener('click',function () {
+   if (darkToggle.checked) {
+      html.classList.add('dark');
+      localStorage.theme = 'dark';
+   }
+   else{
+      html.classList.remove('dark');
+      localStorage.theme = 'light';
+   }
+})
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+   darkToggle.checked = true;
+   
+} else {
+   darkToggle.checked = false;
+ }
